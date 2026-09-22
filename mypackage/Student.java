68aa813eeff62ab9fc7d3d3b9ba558363e0df31e
Yogeetha kolla <ksangeetha58910@gmail.com>
2026-09-22 16:20:34 +0530

    Add Student class with display method

---
+++ mypackage/Student.java
@@ -0,0 +1,12 @@
+package mypackage;
+
+public class Student {
+
+    public void display() {
+        System.out.println("Student Details");
+        System.out.println("----------------");
+        System.out.println("Name: Arun");
+        System.out.println("Age: 20");
+        System.out.println("Course: BCA");
+    }
+}
